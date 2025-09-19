72ff35234de90856bc8c51f0195a30b1ca838a85
Angel Torres <125419049+A4NG31@users.noreply.github.com>
2025-09-19 13:04:47 -0500

    Update FUNCIONA app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+
 # -------------------------
 # Configuración página
 # -------------------------
