92b4fbe1a8bcc5ee1ec4365744a271dfc133ff4c
Angel Torres <125419049+A4NG31@users.noreply.github.com>
2025-09-19 12:55:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ st.markdown("""
 [data-testid="stSidebar"] .stFileUploader .uppy-Dashboard-AddFiles-title,
 [data-testid="stSidebar"] .stFileUploader .uppy-Dashboard-AddFiles-subtitle,
 [data-testid="stSidebar"] .stFileUploader .uppy-Dashboard-AddFiles-list button,
-[data-testid="stSidebar"] .stFileUploader .uppy-Dashboard-Item-name,
-[data-testid="stSidebar"] .stFileUploader .uppy-Dashboard-Item-status,
+#[data-testid="stSidebar"] .stFileUploader .uppy-Dashboard-Item-name,
+#[data-testid="stSidebar"] .stFileUploader .uppy-Dashboard-Item-status,
 [data-testid="stSidebar"] .stFileUploader span,
 [data-testid="stSidebar"] .stFileUploader div {
     color: black !important;
